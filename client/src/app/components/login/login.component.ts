7fcc4a5cb7896b8bcd9e8f05de4e32f1d620bf84
duongtran2003 <dt005250@gmail.com>
2023-07-09 21:21:33 +0700

    login validation implemented

--- client/src/app/components/login/login.component.ts
+++ client/src/app/components/login/login.component.ts
@@ -10,8 +10,9 @@ import { AuthService } from 'src/app/services/auth.service';
 export class LoginComponent {
   userName: string = "";
   userPassword: string = "";
-  userNameError: boolean = false;
-  userPasswordError: boolean = false;
+  userNameError: string = "";
+  userPasswordError: string = "";
+  usernameRegex: RegExp = /^[a-zA-Z0-9_]{6,20}$/;
   @Output() onLoginToggle = new EventEmitter();
   @Output() onRegisterToggle = new EventEmitter();
 
@@ -19,17 +20,31 @@ export class LoginComponent {
   onLoginClick(): void {
 
     //validate input start:
-
+    if (!this.usernameRegex.test(this.userName)) {
+      this.userNameError = "No special characters, length has to be between 6 and 20";
+    }
+    else {
+      this.userNameError = "";
+    }
+    if (this.userPassword.length < 8 || this.userPassword.length > 20) {
+      this.userPasswordError = "Too short or too long";
+    }
+    else {
+      this.userPasswordError = "";
+    }
     //validate input end;
-
+    if (this.userNameError != "" || this.userPasswordError != "") {
+      return;
+    }
     this.api.post('auth/login', {
       username: this.userName,
       password: this.userPassword
     }).subscribe({
       next: (response: any) => {
-        console.log(response.message);
         this.auth.setCurrentUser(response.username); 
         this.onLoginToggle.emit();
+        this.userNameError = "";
+        this.userPasswordError = "";
       },
       error: (err) => {
         console.log(err.statusCode);
