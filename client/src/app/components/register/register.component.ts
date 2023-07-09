efb67701b297fe53dc9d0b4ddd55430952a47315
duongtran2003 <dt005250@gmail.com>
2023-07-09 22:06:39 +0700

    added client side login and register validation

--- client/src/app/components/register/register.component.ts
+++ client/src/app/components/register/register.component.ts
@@ -11,13 +11,57 @@ export class RegisterComponent {
   userName: string = "";
   userPassword: string = "";
   userEmail: string = "";
+  userNameError: string = "";
+  userPasswordError: string = "";
+  userEmailError: string = "";
+  emailRegex: RegExp = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  usernameRegex: RegExp = /^[a-zA-Z0-9_]{6,20}$/;
+  passwordRegex: RegExp = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,20}$/;
+
   @Output() onRegisterToggle = new EventEmitter();
   @Output() onLoginToggle = new EventEmitter();
-  
+
   constructor(private api: ApiService, private auth: AuthService) { }
-  
+
   onRegisterClick(): void {
 
+    //input validation begin:
+    if (!this.emailRegex.test(this.userEmail)) {
+      this.userEmailError = "Invalid email";
+    }
+    else {
+      this.userEmailError = "";
+    }
+    if (!this.usernameRegex.test(this.userName)) {
+      this.userNameError = "No special characters, length has to be between 6 and 20";
+    }
+    else {
+      this.userNameError = "";
+    }
+    if (!this.passwordRegex.test(this.userPassword)) {
+      this.userPasswordError = "Has to contain at least 1 alphabetic character, 1 digit, and between 8 and 20 characters long"
+    }
+    else {
+      this.userPasswordError = "";
+    }
+    if (this.userNameError != "" || this.userPasswordError != "" || this.userEmailError != "") {
+      return;
+    }
+    //input validation end:
+
+    this.api.post('auth/register', {
+      username: this.userName,
+      email: this.userEmail,
+      password: this.userPassword,
+    }).subscribe({
+      next: (response) => {
+        console.log(response.message);
+        this.onLoginToggle.emit();
+      },
+      error: (err) => {
+        console.log(err.message);
+      }
+    })
   }
   onLoginClick(): void {
     this.onLoginToggle.emit();
