5b17a6f2c15fbd1cf09d9f9c99bad0649884dc64
duongtran2003 <dt005250@gmail.com>
2023-07-16 15:52:15 +0700

    avatar tooltip added

--- client/src/app/components/user-profile/user-profile.component.ts
+++ client/src/app/components/user-profile/user-profile.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from 'src/app/services/api.service';
-import { ToastService } from 'src/app/services/toast.service';
 import { ActivatedRoute } from '@angular/router';
+import { Clipboard } from '@angular/cdk/clipboard';
+import { ToastService } from 'src/app/services/toast.service';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-user-profile',
@@ -9,10 +11,12 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./user-profile.component.css']
 })
 export class UserProfileComponent implements OnInit {
-  constructor(private api: ApiService, private toast: ToastService, private route: ActivatedRoute) { }
+  constructor(private auth: AuthService, private clipboard: Clipboard, private api: ApiService, private toast: ToastService, private route: ActivatedRoute) { }
   username: string = "";
   email: string = "";
   avatarPath: string = "";
+  isMyProfile: boolean = false;
+  isTooltipVisible: boolean = false;
   ngOnInit(): void {
     this.route.params.subscribe(params => {
       const usernameFromParams = params['username'];
@@ -23,11 +27,38 @@ export class UserProfileComponent implements OnInit {
           this.username = response.username;
           this.email = response.email;
           this.avatarPath = response.avatarPath;
+          if (response.username == this.auth.getCurrentUser()) {
+            this.isMyProfile = true;
+          }
         },
         error: (err) => {
           console.log(err.error.message);
         }
       })
     })
+  }
+
+  showUpdateAvatarTooltip(): void {
+    this.isTooltipVisible = true;
+  }
+  hideUpdateAvatarTooltip(): void {
+    this.isTooltipVisible = false;
+  }
+
+  onUsernameClick() {
+    this.toast.makeToast({
+      state: "close",
+      message: "Copied to clipboard",
+      barClass: ['bg-blue-500'],
+    });
+    this.clipboard.copy(this.username);
+  }
+  onEmailClick() {
+    this.toast.makeToast({
+      state: "close",
+      message: "Copied to clipboard",
+      barClass: ['bg-blue-500'],
+    });
+    this.clipboard.copy(this.email);
   }
 }
